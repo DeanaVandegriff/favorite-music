6c7ebf5ff34ce04ea70b1de9653ce1de729f91c6
deana <dvandegriff1@gmail.com>
2022-03-14 13:30:34 -0500

    adding favorite music app

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,12 @@
+class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: songs.as_json
+  end
+
+  def show
+    song_id = params["id"]
+    song = Song.find_by(id: song_id)
+    render json: song.as_json
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+
+# song = Song.new(title: "Brown Eyed Girl", album: "Blowin' Your Mind", artist: "Van Morrison", year: 1967)
+# song.save
+
+# song = Song.new(title: "Not Ready to Make Nice", album: "Taking the Long Way", artist: "The Chicks", year: 2006)
+# song.save
+
+# song = Song.new(title: "Half of my Hometown", album: "Kelsea", artist: "Kelsea Ballerini", year: 2020)
+# song.save
+
+# song = Song.new(title: "The Boxer", album: "Bridge Over Troubled Water", artist: "Simon and Garfunkel", year: 1970)
+# song.save
